621117e1f2308c3ac8208011163e2a7e827e1d4c
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-01-11 11:12:13 +0400

    generating names

---
+++ name.py
@@ -0,0 +1,52 @@
+import random as rn
+
+names = [
+    "Alexander",
+    "Dmitry",
+    "Sergey",
+    "Mikhail",
+    "Andrey",
+    "Nikolay",
+    "Vladimir",
+    "Pavel",
+    "Ivan",
+    "Roman",
+]
+
+surnames = [
+    "Ivanov",
+    "Petrov",
+    "Smirnov",
+    "Kuznetsov",
+    "Popov",
+    "Sokolov",
+    "Lebedev",
+    "Kozlov",
+    "Novikov",
+    "Morozov",
+    "Volkov",
+    "Fedorov",
+    "Mikhailov",
+    "Belov",
+    "Tarasov",
+    "Orlov",
+    "Gusev",
+    "Karpov",
+    "Zaitsev",
+    "Solovyov"
+]
+
+
+
+def generate_name() -> str:
+    name = rn.choice(seq=names)
+    return name
+
+def generate_surname() -> str:
+    surname = rn.choice(seq=surnames)
+    return surname
+
+
+def generate_fullname() -> str:
+    return f"{generate_name()} {generate_surname()}"
+
